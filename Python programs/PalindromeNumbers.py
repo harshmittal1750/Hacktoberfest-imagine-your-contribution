a22112fcf6317ffc46c6c91287bf02afcc93e697
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-10-02 23:17:16 +0530

    Update PalindromeNumbers.py

--- Python programs/PalindromeNumbers.py
+++ Python programs/PalindromeNumbers.py
@@ -1,13 +1,10 @@
-try:
-    def solve(n):
+def solve(n):
         rev=n[::-1]
         print(rev)
         if n==rev:
             return "YES"
         else:
             return "NO"
-    if __name__=="__main__":
-        n=input("Enter Number ")
-        print(solve(n))
-except:
-    pass
+n=input("Enter Number ")
+print(solve(n))
+
