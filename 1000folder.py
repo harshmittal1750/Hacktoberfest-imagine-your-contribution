6e1f82273ed54d63ba346048bf81429d59d12809
Harsh Mittal <harshmittal1750@gmail.com>
2021-10-03 20:59:34 +0530

    fixes

--- 1000folder.py
+++
@@ -1,4 +0,0 @@
-import os
-path = os.getcwd()
-for i in range(1,1001):
-    os.mkdir(path + f"\\{i}")
---
+++ Python programs/1000folder.py
@@ -0,0 +1,4 @@
+import os
+path = os.getcwd()
+for i in range(1,1001):
+    os.mkdir(path + f"\\{i}")
