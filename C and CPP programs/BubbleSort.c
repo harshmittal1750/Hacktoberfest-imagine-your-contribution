7f91fd7e92b209381f91397a7625f1d20cc3efef
Prakhar Agarwal <71598512+prakhar07msd@users.noreply.github.com>
2021-10-03 12:21:38 +0530

    Create BubbleSort.c

---
+++ C and CPP programs/BubbleSort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void bubblesort(int arr[],int n)
+{
+    int i,j;
+    for(int i=0;i<n-1;i++)
+    {
+        for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+    }
+}
+
+void printarray(int arr[],int size)
+{
+    int i;
+    for(int i=0;i<size;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[]={69,43,7,45,27};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,n);
+    printf("Sorted array: \n");
+    printarray(arr,n);
+    return 0;
+}
