f4c8c7fce9c16f399ed777ff2e99bc6b3d5538c7
Priya Rawat <83421508+priyaa-rawat@users.noreply.github.com>
2021-10-12 12:14:11 +0530

    Update retrieve_all_urls_from_given_webpage.py

--- Python programs/retrieve_all_urls_from_given_webpage.py
+++ Python programs/retrieve_all_urls_from_given_webpage.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
-ctx=ssl.create_default_context
+ctx=ssl.create_default_context ()
 ctx.check_hostname=False
 ctx.verify_mode=ssl.CERT_NONE
 
